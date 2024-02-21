cdc1a113cd9d7e9c4f25ba2214b6c08f455cc69a
Pcykho Duke <62355234+Pcykho@users.noreply.github.com>
2024-02-21 16:16:19 +0530

    Added the JavaScript code files
    Run it on your browser or any other JS supported console.

---
+++ Dog age to Human Age Formula using JavaScript.js
@@ -0,0 +1,7 @@
+// Dog age to Human Age Formula using JavaScript
+// Special thanks to Angela Yu (https://github.com/angelabauer)
+
+var dogAge = prompt ("Your Dog's Age:");
+var humanAge = ((dogAge - 2) * 4 + 21);
+
+alert ( "Your Dog's human age is " + humanAge + " years old");
